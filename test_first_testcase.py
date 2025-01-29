760cb92192eca5e8921571f919b53fd8ccd494dd
Nesar <nesarh24@gmail.com>
2025-01-29 20:02:26 +0530

    first_commit

---
+++ test_first_testcase.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from utilities.Baseclass import Baseclass
+from Page_Objects.HomePage import Home
+
+class Test_First(Baseclass):
+    def test_first(self):
+        log = self.get_logger()
+        # Land on shopping cart page
+        home = Home(self.driver)
+        checkout = home.shop_item()
+        log.info("Getting the info from the shopping page")
+        # Add items to cart
+        product = checkout.get_card_titles()
+        for i in product:
+            log.info(i.text)
+            if i.text == "Blackberry":
+                checkout.get_card_footer().click()
+                break
+        log.info("Item added to cart")
+        # Go to cart
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollTop);")
+        checkout.get_cart_btn().click()
+        log.info("Navigated to checkout page")
+        # Click on checkout
+        confirm = checkout.get_checkout_btn()
+        log.info("Navigated to confirm page")
+        # Enter country, click on checkbox and click on purchase
+        confirm.get_country().send_keys("ind")
+        self.exp_wait("India")
+        confirm.get_select_country().click()
+        log.info("Country selected")
+        confirm.get_checkbox().click()
+        log.info("Checkbox selected")
+        confirm.get_purchase().click()
+        log.info("Purchased")
+        # Verify a success message
+        self.success()
+        # wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".alert-success")))
+        text_match = confirm.get_text().text
+        log.info("Text received: " + text_match)
+        assert "Thank you!" in text_match
+
---
+++ test_homepage.py
@@ -0,0 +1,34 @@
+import pytest
+from Page_Objects.HomePage import Home
+from Testdata.Home_page_data import home1
+
+from utilities.Baseclass import Baseclass
+
+class Test_Home(Baseclass):
+    def test_form(self, data):
+        log = self.get_logger()
+        home = Home(self.driver)
+        home.get_name().send_keys(data["first_name"])
+        log.info("First name entered: "+data["first_name"])
+        # home.get_mail().send_keys(data["mail"])
+        # log.info("Email entered: "+data["mail"])
+        # home.get_password().send_keys(data["password"])
+        # log.info("Password entered: "+data["password"])
+        home.get_check().click()
+        log.info("Checkbox selected")
+        self.dropdown(home.get_drop_down(), data["gender"])
+        log.info("Gender selected: "+data["gender"])
+        home.get_radio().click()
+        log.info("Radio button selected")
+        home.get_submit().click()
+        log.info("Submit button clicked")
+        message_conf = home.get_alert_text().text
+        log.info("Alert message captured is:" + message_conf)
+        self.driver.refresh()
+        assert "Success" in message_conf
+        log.info("Page refreshed")
+
+
+    @pytest.fixture(params=home1.get_data())
+    def data(self, request):
+        return request.param
